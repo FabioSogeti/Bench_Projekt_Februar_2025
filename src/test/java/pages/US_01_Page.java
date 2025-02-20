e6f97fd389265be5eb506cc49eb197f18232638e
Fabio Marques da Silva <fabio.marques-da-silva@sogeti.com>
2025-02-20 13:37:33 +0100

    Ich habe die US_01_Page und die US_01_Test erweitert.

--- src/test/java/pages/US_01_Page.java
+++ src/test/java/pages/US_01_Page.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -15,4 +16,37 @@ public class US_01_Page {
    // This locator is just an example. it must be changed to run any tests
     @FindBy(xpath = "//div[@class='stSelectbox']")
     public WebElement elementsCard;
+
+
+    // Dropdown-Feld für die Datensatzauswahl
+    @FindBy(xpath = "//*[@id=\"root\"]/div[1]/div[1]/div/div/section[2]/div[1]/div/div/div/div[4]/div/div/div/div[1]")
+    public WebElement dropdown_datentypauswahl;
+
+
+    // Auswählbare Optionen des dropdownfelds für die Datensatzauswahl
+    @FindBy(xpath = "//*[@id=\"root\"]/div[2]/div/div/div/div/div/div/ul/div/div/li[1]")
+    public WebElement datentypauswahl_registrierung;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div[2]/div/div/div/div/div/div/ul/div/div/li[2]")
+    public WebElement datentypauswahl_login;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div[2]/div/div/div/div/div/div/ul/div/div/li[3]")
+    public WebElement datentypauswahl_profil;
+
+
+    // Eingabefeld der gewünschten Datensatzanzahl
+    @FindBy(xpath = "//*[@id=\"root\"]/div[1]/div[1]/div/div/section[2]/div[1]/div/div/div/div[5]/div/div/div[1]/div/input")
+    public WebElement inputfeld_anzahl_datensaetze;
+
+
+    // Button zum generieren der Datensätze
+    @FindBy(xpath = "//*[@id=\"root\"]/div[1]/div[1]/div/div/section[2]/div[1]/div/div/div/div[6]/div/button")
+    public WebElement button_daten_generieren;
+
+
+    // Tabelle, welche die Ergebnisse der Datensatzgenerierung anzeigt
+    @FindBy(xpath = "//*[@id=\"root\"]/div[1]/div[1]/div/div/section[2]/div[1]/div/div/div/div[8]/div/div/div[2]/div[1]/div/div/div[1]/canvas[1]/table")
+    public WebElement table_generierte_daten;
+
+
 }
--- src/test/java/tests/US_01_Test.java
+++ src/test/java/tests/US_01_Test.java
@@ -3,18 +3,32 @@ package tests;
 import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import utilities.ConfigReader;
-import utilities.Driver;
-import utilities.ReusableMethods;
+import pages.US_01_Page;
 import utilities.TestBase;
 
 public class US_01_Test extends TestBase {
 
-    //This Test is just an example. It needs to be changed to properly run the tests.
+    //This Test generates 5 data sets of the "registrieren" type and checks if they are shown
     @Test
     public void TC_01() {
-        us01Page.elementsCard.click();
-        ReusableMethods.waitFor(2);
-        Assert.assertFalse(us01Page.elementsCard.isSelected());
+
+        US_01_Page us01Page = new US_01_Page();
+
+        // Öffnet die dropdown-Liste zur Auswahl des Datensatzes.
+        us01Page.dropdown_datentypauswahl.click();
+
+        // Wählt die option "registrierung" in der Datensatz dropdown-Liste und bestätigt diese.
+        us01Page.datentypauswahl_registrierung.click();
+        Assert.assertEquals(us01Page.dropdown_datentypauswahl.getText(), "registrierung");
+
+        //Setzt die Anzahl der generierte Datensetze auf 5 und bestätigt diese.
+        //.clear() methode funktioniert aus irgendeinem Grund nicht und daher wird die Eingabe von einem "Backspace" simuliert. Gerne verbessern sofern möglich.
+        us01Page.inputfeld_anzahl_datensaetze.sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, "5", Keys.ENTER);
+        Assert.assertEquals(us01Page.inputfeld_anzahl_datensaetze.getAttribute("value"), "5");
+
+        //Generiert die Datensätze und überprüft deren Anzahl anhand der Anzahl an Reihen in der Tabelle
+        //Die Tabelle checkt nach einer Reihe mehr aufgrund der ersten Reihe, die als Überschrift des Datentyps fungiert.
+        us01Page.button_daten_generieren.click();
+        Assert.assertEquals(us01Page.table_generierte_daten.getAttribute("aria-rowcount"), "6");
     }
 }
