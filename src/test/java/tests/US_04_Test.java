bf0b6e2c13e815929b0b7c7afb714182be5bf3e2
zhrOsm <zhr.osmanogullari@gmail.com>
2025-02-20 11:13:45 +0100

    first test

---
+++ src/test/java/tests/US_04_Test.java
@@ -0,0 +1,37 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.US_01_Page;
+import pages.US_04_Page;
+import utilities.Driver;
+import utilities.TestBase;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+    public class US_04_Test extends TestBase {
+
+        US_04_Page us04Page = new US_04_Page();
+        //private WebDriver driver;
+        //private String baseURL = "http://localhost:8501/"; // Lokale URL anpassen
+        @Test(priority = 1)
+        public void testStartWithoutInternet() {
+            System.out.println("Bitte vorher die Internetverbindung deaktivieren!");
+            // Prüfen, ob die Anwendung geladen wurde
+            WebElement header = us04Page.title; // Beispiel für Überschrift auf der Seite
+            Assert.assertNotNull(header, "Die Anwendung konnte nicht ohne Internetverbindung gestartet werden.");
+        }
+
+    }
+
+
